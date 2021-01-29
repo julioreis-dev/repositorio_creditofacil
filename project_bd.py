524626a6792725585140d493509e1c0e05033948
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2021-01-29 17:15:52 -0300

    Add files via upload

--- project_bd.py
+++ project_bd.py
@@ -17,52 +17,49 @@ class CreateBD:
             mycursor = connection.cursor()
             mycursor.execute(f"CREATE DATABASE {database_name}")
         except mysql.connector.errors.DatabaseError:
-            return f'Já existe um Banco de dados com nome "{database_name}" que será utilizado no projeto!!!'
+            return f'Banco de dados "{database_name}" já existente e em uso!!!'
         else:
-            return 'Banco de dados criado com sucesso!!!'
+            return f'Banco de dados "{database_name}" criado com sucesso!!!'
 
-
-    def create_client_table(self):
+    def create_tables(self, list_instr):
         connection = mysql.connector.connect(host=self.host, database=self.database, user=self.user,
                                              password=self.password, port=self.port)
-        cursor = connection.cursor()
-        sql = """CREATE TABLE IF NOT EXISTS clientes (
-           idclientes INT NOT NULL AUTO_INCREMENT,
-           cpf VARCHAR(11) NOT NULL,
-           nome_completo VARCHAR(100) NOT NULL,
-           nascimento VARCHAR(10) NOT NULL,
-           email VARCHAR(100) NOT NULL,
-           telefone VARCHAR(11) NOT NULL,
-           salario FLOAT NOT NULL,
-           proposta_enviada DATETIME NULL,
-           idproposta INT NULL,
-           status_proposta VARCHAR(30) NULL,
-           PRIMARY KEY (idclientes))"""
-        cursor.execute(sql)
-        connection.close()
-
-    def create_list_offer_table(self):
-        connection = mysql.connector.connect(host=self.host, database=self.database, user=self.user,
-                                             password=self.password, port=self.port)
-        cursor = connection.cursor()
-        sql = """CREATE TABLE IF NOT EXISTS list_ofertas (
-                idofertas INT NOT NULL AUTO_INCREMENT,
-                ofertas JSON NULL,
-                inclusao DATETIME NULL,
-                PRIMARY KEY (idofertas))"""
-        cursor.execute(sql)
+        for query in list_instr:
+            cursor = connection.cursor()
+            sql = query
+            cursor.execute(sql)
         connection.close()
 
 
-env = Env()
-env.read_env()
-host = env.str('NT_BD_HOST')
-database = env.str('NT_BD_DATABASE')
-user = env.str('NT_BD_USER')
-password = env.str('NT_BD_PASSWORD')
-port = env.int('NT_BD_PORT')
-db = CreateBD(host, database, user, password, port)
-return_db = db.create_database(database)
-db.create_client_table()
-db.create_list_offer_table()
-print(return_db)
+def main():
+    sql1 = """CREATE TABLE IF NOT EXISTS clientes (
+               idclientes INT NOT NULL AUTO_INCREMENT,
+               cpf VARCHAR(11) NOT NULL,
+               nome_completo VARCHAR(100) NOT NULL,
+               nascimento VARCHAR(10) NOT NULL,
+               email VARCHAR(100) NOT NULL,
+               telefone VARCHAR(11) NOT NULL,
+               salario FLOAT NOT NULL,
+               proposta_enviada DATETIME NULL,
+               idproposta INT NULL,
+               status_proposta VARCHAR(30) NULL,
+               PRIMARY KEY (idclientes))"""
+
+    sql2 = """CREATE TABLE IF NOT EXISTS list_ofertas (
+                    idofertas INT NOT NULL AUTO_INCREMENT,
+                    ofertas JSON NULL,
+                    inclusao DATETIME NULL,
+                    PRIMARY KEY (idofertas))"""
+
+    list_sql = [sql1, sql2]
+    env = Env()
+    env.read_env()
+    host = env.str('NT_BD_HOST')
+    database = env.str('NT_BD_DATABASE')
+    user = env.str('NT_BD_USER')
+    password = env.str('NT_BD_PASSWORD')
+    port = env.int('NT_BD_PORT')
+    db = CreateBD(host, database, user, password, port)
+    return_db = db.create_database(database)
+    db.create_tables(list_sql)
+    print(return_db)
